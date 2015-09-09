a5d0f74ce619f935ca6173f9d8fd36b306dcfc15
degtyarjov <degtyarjov@haulmont.com>
2015-09-09 12:49:37 +0400

    add resource reading

--- src/main/java/com/haulmont/mvc/jsf/DataService.java
+++ src/main/java/com/haulmont/mvc/jsf/DataService.java
@@ -2,12 +2,18 @@ package com.haulmont.mvc.jsf;
 
 import com.haulmont.mvc.pojo.User;
 import com.haulmont.mvc.services.DataServiceBean;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.servlet.ServletContext;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author degtyarjov
@@ -17,6 +23,9 @@ import java.util.List;
 @Component
 @SessionScoped
 public class DataService {
+    @Autowired
+    ServletContext servletContext;
+
     @Autowired
     protected DataServiceBean dataServiceBean;
 
@@ -30,5 +39,21 @@ public class DataService {
 
     public List<User> getUsers() {
         return dataServiceBean.getUsers();
+    }
+
+    public String getInfo() {
+        InputStream resourceAsStream = servletContext.getResourceAsStream("/WEB-INF/info.txt");
+        String info = null;
+        try {
+            info = IOUtils.toString(resourceAsStream);
+        } catch (IOException e) {
+            throw new RuntimeException("An error occurred while reading info.txt", e);
+        }
+        return info;
+    }
+
+    public List<String> getResourcesList() {
+        Set<String> resourcePaths = servletContext.getResourcePaths("/WEB-INF/resources/");
+        return new ArrayList<>(resourcePaths);
     }
 }
