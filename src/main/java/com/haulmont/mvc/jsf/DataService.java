1a62def4ec097cd10f0d9cf4cb18224da57e8577
degtyarjov <degtyarjov@haulmont.com>
2015-09-14 12:18:56 +0400

    get resources

--- src/main/java/com/haulmont/mvc/jsf/DataService.java
+++ src/main/java/com/haulmont/mvc/jsf/DataService.java
@@ -4,7 +4,9 @@ import com.haulmont.mvc.pojo.User;
 import com.haulmont.mvc.services.DataServiceBean;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.support.ServletContextResourcePatternResolver;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -53,7 +55,19 @@ public class DataService {
     }
 
     public List<String> getResourcesList() {
-        Set<String> resourcePaths = servletContext.getResourcePaths("/WEB-INF/resources/");
-        return new ArrayList<>(resourcePaths);
+//        Set<String> resourcePaths = servletContext.getResourcePaths("/WEB-INF/resources/");
+//        return new ArrayList<>(resourcePaths);
+        try {
+            ServletContextResourcePatternResolver patternResolver = new ServletContextResourcePatternResolver(servletContext);
+            Resource[] resources = patternResolver.getResources("/WEB-INF/resources/**/*.txt");
+            List<String> resourcesNames = new ArrayList<>();
+            for (Resource resource : resources) {
+                resourcesNames.add(resource.getFilename());
+            }
+
+            return resourcesNames;
+        } catch (IOException e) {
+            throw new RuntimeException("An error occurred while reading resources", e);
+        }
     }
 }
